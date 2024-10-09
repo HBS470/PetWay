5049287faaf27fbf3886caa57ad50d98b1803745
Horeb SILVA <horeb.silva241004@gmail.com>
2024-10-09 16:20:58 +0200

    bdd

--- modules/connexionBD/connexionBD.php
+++ modules/connexionBD/connexionBD.php
@@ -1,3 +1,51 @@
 <?php
-echo 'Hello, World!';
+
+if (!defined('MY_APP')) {
+    exit('Accès non authorisé');
+}
+    Class Connexion {
+        protected static $bdd;
+        public function __construct() {
+            $this->connexion();
+        }
+        public function connexion() {
+            try {
+                self::$bdd = new PDO('mysql:host=localhost;dbname=petway', 'horeb', 'horeb');
+                self::$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo 'Connexion réussie à la base de données!';
+            } catch (PDOException $e) {
+                die('Erreur de connexion à la base de données : ' . $e->getMessage());
+            }
+        }
+
+        function checkCSRFToken(){
+            if(isset($_POST['csrf_token'])) {
+                $user_token = $_POST['csrf_token'];
+                if($user_token === $_SESSION['csrf_token']) {
+                    return true;
+                } else {
+                    echo "Erreur : Jeton CSRF non valide.";
+                    return false;
+                }
+
+            }else if(isset($_GET['csrf_token'])) {
+                $user_token = $_GET['csrf_token'];
+                if($user_token === $_SESSION['csrf_token']) {
+                    return true;
+                } else {
+                    echo "Erreur : Jeton CSRF non valide.";
+                    return false;
+                }
+
+            } else {
+                echo "Erreur : Jeton CSRF manquant dans la requête.";
+                return false;
+            }
+        }
+
+        static function generateCSRFToken() {
+            return bin2hex(random_bytes(32));
+        }
+    }
+    
 ?>
