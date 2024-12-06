fa9b5368528f7df858187db530633e052e7393b2
Horeb SILVA <113896432+HBS470@users.noreply.github.com>
2024-12-06 11:46:46 +0100

    modif nom css et js

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 		<meta charset= "UTF-8"/>
 		<title>PetWay</title>
 		<!-- UTILISATION DE BOOTSTRAP-->
-		<link rel="stylesheet" href="css+js/bootstrap.min.css">
+		<link rel="stylesheet" href="css+js/bootstrap.css">
 		<link rel="stylesheet" href="css+js/style.css">
-		<script src="css+js/bootstrap.min.js"></script>
+		<script src="css+js/bootstrap.js"></script>
 	</head>
 	<body>
 	<?php
