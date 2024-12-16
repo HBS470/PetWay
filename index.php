3e5c81aea0c89dc9d7931f8be16f7e28bbc7339b
Horeb SILVA <horeb.silva241004@gmail.com>
2024-12-16 11:06:35 +0100

    clean up

--- index.php
+++ index.php
@@ -4,11 +4,35 @@
 		<meta charset= "UTF-8"/>
 		<title>PetWay</title>
 		<link rel="stylesheet" href="css+js/style.css">
+        <script src="css+js/script.js"></script>
 	</head>
 	<body>
 	<?php
-	
-	?>
+    session_start();
+
+    include_once "header.php";
+
+    if (isset($_GET['module'])) {
+        $module = $_GET['module'];
+
+        switch ($module) {
+
+
+            default:
+                echo 'Aucun module detecté !';
+                break;
+        }
+    }
+    else {
+        include_once "accueil.php";
+    }
+
+    include_once "footer.php";
+    function generateCSRFToken() {
+        return bin2hex(random_bytes(32));
+    }
+
+    ?>
 
     </body>
 
